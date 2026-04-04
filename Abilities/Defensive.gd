740d284a799550a7dd92cec502d31fdfdf536f50
Morgan <39073323+Morgan-Steele@users.noreply.github.com>
2026-04-04 17:18:55 -0600

    Progress bar

--- Abilities/Defensive.gd
+++ Abilities/Defensive.gd
@@ -3,16 +3,12 @@ class_name Defensive
 var base_block
 var luck
 
-func _init(t_name,t_skill,t_aptitude,t_like, bblock=skill/5, lucky=20):
+func _init(t_name, t_skill, t_aptitude, t_like, bblock=10, lucky=20):
 	super._init(t_name,t_skill,t_aptitude,t_like)
 	base_block=bblock
 	luck=lucky
+
 func use(enemy):
 	var blocked = base_block + randi_range(0, luck)
 	var damage = enemy.attack()
 	StatsManager.health -= max(0, damage - blocked)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
