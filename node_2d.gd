4e3fe06b8b92e389d8d212508e7a5154bdd9c799
1123581321345589144233377610987 <swirlthree@gmail.com>
2026-04-04 02:30:38 -0600

    a start

---
+++ node_2d.gd
@@ -0,0 +1,32 @@
+extends Node2D
+func train_ability(Ability ability, Line line):
+	if(line.obedience+(ability.like*training_level)>50):
+		ability.skill+=ability.aptitude
+
+class_name Line:
+	var color
+	var texture #enum (solid, dashed, dotted, bold)
+	var end #enum (none, ball, arrow, diamond)
+	var affection #int 0-100
+	var obedience #int 0-100
+	func _init():
+		
+	
+class_name Ability:
+	var name #string
+	var skill #int 0-100 - how good line is at it
+	var aptitude #int 0-20? idk. It's how much the line improves each time it trains it
+	var training_level #int num times trained
+	var like #int -2 through 2 maybe? How much it likes it
+		func _init():
+			training_level=0
+	
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
